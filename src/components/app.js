dc5e5375a81c2354ca36ce1d549bbf8d1cec1335
rfigueror1 <rfigueror@gmail.com>
2018-06-30 12:02:14 -0700

    Search bar clears when button is clicked

--- src/components/app.js
+++ src/components/app.js
@@ -2,18 +2,26 @@ angular.module('video-player')
   .component('app', {
     controller: function(youTube, $http) {
       var thisApp = this;
-      thisApp.videos = exampleVideoData;
-      thisApp.currentVideo = exampleVideoData[0];
+      // thisApp.videos = exampleVideoData;
+      // thisApp.currentVideo = exampleVideoData[0];
       thisApp.result = '';
       
       thisApp.selectVideo = function(newCurrentVideo) {
         thisApp.currentVideo = newCurrentVideo;
       };
       
-      thisApp.searchResults = function(newResult) {
+      thisApp.searchResults = function(newResult, blank) {
         thisApp.result = newResult;
         youTube.search(newResult, thisApp);
+        thisApp.result = blank;
       };
+      
+      thisApp.init = function() {
+        console.log('init');
+        thisApp.searchResults('');
+      };
+      
+      thisApp.init();
     },
     templateUrl: 'src/templates/app.html'
   });
