0def8b7a8fcc50026e679b2d3f036ddfaf203dce
rfigueror1 <rfigueror@gmail.com>
2018-06-30 14:52:37 -0700

    App fully functioning

--- src/components/app.js
+++ src/components/app.js
@@ -2,18 +2,18 @@ angular.module('video-player')
   .component('app', {
     controller: function(youTube, $http) {
       var thisApp = this;
-      // thisApp.videos = exampleVideoData;
-      // thisApp.currentVideo = exampleVideoData[0];
+      thisApp.videos = exampleVideoData;
+      thisApp.currentVideo = exampleVideoData[0];
       thisApp.result = '';
       
       thisApp.selectVideo = function(newCurrentVideo) {
         thisApp.currentVideo = newCurrentVideo;
       };
       
-      thisApp.searchResults = function(newResult, blank) {
+      thisApp.searchResults = function(newResult) {
         thisApp.result = newResult;
         youTube.search(newResult, thisApp);
-        thisApp.result = blank;
+        thisApp.result = '';
       };
       
       thisApp.init = function() {
